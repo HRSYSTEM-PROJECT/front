d502c131e944a412e371b80b0bc1c510d9c27a17
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-10-06 14:42:16 -0300

    modificaciones en redireccion al login

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,21 +13,19 @@ export default function DashboardLayout({
   const router = useRouter();
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
 
-  useEffect(() => {
-    if (!isLoading && !isAuthenticated) {
-      router.push("/login");
-    }
-  }, [isAuthenticated, isLoading, router]);
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        Cargando sesión...
-      </div>
-    );
-  }
-
-  if (!isAuthenticated) return null;
+  // useEffect(() => {
+  //   if (!isLoading && !isAuthenticated) {
+  //     router.push("/login");
+  //   }
+  // }, [isAuthenticated, isLoading, router]);
+  // if (isLoading) {
+  //   return (
+  //     <div className="flex items-center justify-center h-screen">
+  //       Cargando sesión...
+  //     </div>
+  //   );
+  // }
+  // if (!isAuthenticated) return null;
 
   return (
     <div className="flex min-h-screen transition-all duration-300">
