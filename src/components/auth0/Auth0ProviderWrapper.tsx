117c9f5a931b3aa8505f049150544ee739364b72
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-10-09 18:06:04 -0300

    implementacion de auth0

--- src/components/auth0/Auth0ProviderWrapper.tsx
+++ src/components/auth0/Auth0ProviderWrapper.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN || "";
-const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENTID || "";
+const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || "";
 
 export function Auth0ProviderWrapper({
   children,
