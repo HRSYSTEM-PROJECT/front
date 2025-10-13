f1b958e342095b26adef8dde88df2b8d5fa0b392
EFedericoTolentino <fedetolentinocr@gmail.com>
2025-10-13 17:24:35 -0300

    notificaciones update v2

--- src/app/(dashboard)/registroEmpleados/page.tsx
+++ src/app/(dashboard)/registroEmpleados/page.tsx
@@ -221,6 +221,7 @@ export default function RegistroEmpleadosPage() {
         });
       }
     }
+    console.log("hola");
 
     return (
       <div className="container mx-auto p-4 sm:p-6 text-start">
